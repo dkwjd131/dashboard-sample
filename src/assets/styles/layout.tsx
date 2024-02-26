f23def672cd5f5c209ab1e6a9fdcce939a037822
dkwjd131 <dkwjd131@naver.com>
2024-02-26 16:53:06 +0900

    design : 스타일 폴더 및 emotion css 생성

---
+++ src/assets/styles/layout.tsx
@@ -0,0 +1,18 @@
+import { css } from '@emotion/react';
+
+export const header = {
+  container: css({
+    boxShadow: '0px 1px 5px 0px #b8b8bd',
+    height: '50px',
+    display: 'flex',
+    alignItems: 'center',
+    padding: '0px 20px 0px 20px',
+  }),
+  wrapper: css({}),
+};
+
+export const main = {
+  container: css({
+    background: '#b8b8bd',
+  }),
+};
