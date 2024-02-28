2db89f84c3b35a49ee3f301dc67e5452eff597e2
dkwjd131 <dkwjd131@naver.com>
2024-02-28 15:44:54 +0900

    chore :  className label format 설정

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   babel: {
-    presets: ['@emotion/babel-preset-css-prop'],
+    presets: [
+      [
+        '@emotion/babel-preset-css-prop',
+        {
+          autoLabel: 'dev-only', // 기본값 'dev-only'
+          labelFormat: 'marvin-[dirname]-[filename]-[local]',
+        },
+      ],
+    ],
   },
 };
