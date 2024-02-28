f0389e895c86d15d5b9cd5a0a8aa8086db88269a
dkwjd131 <dkwjd131@naver.com>
2024-02-28 16:24:40 +0900

    design : 스타일 선언 방식 변경 -> 문자형

--- src/assets/styles/layout.styled.ts
+++ src/assets/styles/layout.styled.ts
@@ -1,20 +1,20 @@
 import { css } from '@emotion/react';
 
 const header = {
-  container: css({
-    boxShadow: '0px 1px 5px 0px #b8b8bd',
-    height: '50px',
-    display: 'flex',
-    alignItems: 'center',
-    padding: '0px 20px 0px 20px',
-  }),
+  container: css`
+    box-shadow: 0px 1px 5px 0px #b8b8bd;
+    height: 50px;
+    display: flex;
+    alignitems: center;
+    padding: 0px 20px 0px 20px;
+  `,
 };
 
 const main = {
-  container: css({
-    height: 'calc(100% - 110px)',
-    padding: '30px',
-  }),
+  container: css`
+    height: calc(100% - 110px);
+    padding: 30px;
+  `,
 };
 
 export { header, main };
--- src/pages/Dashboard/monitoring.styled.ts
+++ src/pages/Dashboard/monitoring.styled.ts
@@ -1,10 +1,10 @@
 import { css } from '@emotion/react';
 
 const monitor = {
-  container: css({
-    background: '#ebf0f5',
-    height: '100%',
-  }),
+  container: css`
+    background: #ebf0f5;
+    height: 100%;
+  `,
 };
 
 export { monitor };
