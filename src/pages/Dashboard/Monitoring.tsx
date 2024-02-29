227661ee9110b72c7929e9b9782bf62935ddf92d
dkwjd131 <dkwjd131@naver.com>
2024-02-29 09:00:41 +0900

    feat: 차트 및 모달 생성

--- src/pages/Dashboard/Monitoring.tsx
+++ src/pages/Dashboard/Monitoring.tsx
@@ -1,20 +1,233 @@
+import React, { useEffect, useState } from 'react';
 import { monitor } from './monitoring.styled';
-import { Box, Grid } from '@mui/material';
+import { Paper, Grid } from '@mui/material';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+  BarChart,
+  Bar,
+  PieChart,
+  Pie,
+} from 'recharts';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
 
 const Monitoring = () => {
+  const [group, setGroup] = useState('G01');
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const lineData = [
+    {
+      name: 'Page A',
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: 'Page B',
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: 'Page C',
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: 'Page D',
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: 'Page E',
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: 'Page F',
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: 'Page G',
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
+  const barData = [
+    {
+      name: 'Page A',
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: 'Page B',
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: 'Page C',
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: 'Page D',
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: 'Page E',
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: 'Page F',
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: 'Page G',
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
+  const pieData01 = [
+    { name: 'Group A', value: 400 },
+    { name: 'Group B', value: 300 },
+    { name: 'Group C', value: 300 },
+    { name: 'Group D', value: 200 },
+  ];
+  const pieData02 = [
+    { name: 'A1', value: 100 },
+    { name: 'A2', value: 300 },
+    { name: 'B1', value: 100 },
+    { name: 'B2', value: 80 },
+    { name: 'B3', value: 40 },
+    { name: 'B4', value: 30 },
+    { name: 'B5', value: 50 },
+    { name: 'C1', value: 100 },
+    { name: 'C2', value: 200 },
+    { name: 'D1', value: 150 },
+    { name: 'D2', value: 50 },
+  ];
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setGroup(event.target.value);
+  };
+  const handleOpen = () => setModalOpen(true);
+  const handleClose = () => setModalOpen(false);
+
   return (
     <div css={monitor.container}>
-      <Grid container>
-        <Grid item columns={4}>
-          1
+      <div css={monitor.inputField}>
+        <div>
+          <Select value={group} label="group" onChange={handleChange}>
+            <MenuItem value={'G01'}>연구개발부</MenuItem>
+            <MenuItem value={'G02'}>사업부</MenuItem>
+            <MenuItem value={'G03'}>경영기획부</MenuItem>
+          </Select>
+        </div>
+        <div>
+          <Button onClick={handleOpen}>Open modal</Button>
+          <Modal open={modalOpen} onClose={handleClose}>
+            <div css={monitor.modal}>모달 안의 내용</div>
+          </Modal>
+        </div>
+      </div>
+      <div css={monitor.chartGrid}>
+        <Grid container columns={12}>
+          <Grid item xs={12} md={4}>
+            <Paper>
+              <ResponsiveContainer width="100%" height="100%">
+                <LineChart
+                  width={500}
+                  height={300}
+                  data={lineData}
+                  margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                  }}
+                >
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="name" />
+                  <YAxis />
+                  <Tooltip />
+                  <Legend />
+                  <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+                  <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+                </LineChart>
+              </ResponsiveContainer>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Paper>
+              <ResponsiveContainer width="100%" height="100%">
+                <BarChart
+                  width={500}
+                  height={300}
+                  data={barData}
+                  margin={{
+                    top: 20,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                  }}
+                >
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="name" />
+                  <YAxis />
+                  <Tooltip />
+                  <Legend />
+                  <Bar dataKey="pv" stackId="a" fill="#8884d8" />
+                  <Bar dataKey="uv" stackId="a" fill="#82ca9d" />
+                </BarChart>
+              </ResponsiveContainer>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Paper>
+              <ResponsiveContainer width="100%" height="100%">
+                <PieChart width={400} height={400}>
+                  <Pie data={pieData01} dataKey="value" cx="50%" cy="50%" outerRadius={60} fill="#8884d8" />
+                  <Pie
+                    data={pieData02}
+                    dataKey="value"
+                    cx="50%"
+                    cy="50%"
+                    innerRadius={70}
+                    outerRadius={90}
+                    fill="#82ca9d"
+                    label
+                  />
+                </PieChart>
+              </ResponsiveContainer>
+            </Paper>
+          </Grid>
         </Grid>
-        <Grid item columns={4}>
-          2
-        </Grid>
-        <Grid item columns={4}>
-          3
-        </Grid>
-      </Grid>
+      </div>
     </div>
   );
 };
--- src/pages/Dashboard/monitoring.styled.ts
+++ src/pages/Dashboard/monitoring.styled.ts
@@ -5,6 +5,40 @@ const monitor = {
     background: #ebf0f5;
     height: 100%;
   `,
+  inputField: css`
+    padding: 20px 20px 5px 20px;
+    display: flex;
+    align-items: center;
+    gap: 20px;
+  `,
+  chartGrid: css`
+    padding: 10px;
+    height: 500px;
+
+    & .MuiGrid-container {
+      height: 100%;
+
+      .MuiGrid-item {
+        padding: 10px;
+        height: 100%;
+
+        .MuiPaper-root {
+          height: 100%;
+          padding: 10px;
+        }
+      }
+    }
+  `,
+  modal: css`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 400px;
+    height: 400px;
+    background: white;
+    padding: 30px 20px;
+  `,
 };
 
 export { monitor };
